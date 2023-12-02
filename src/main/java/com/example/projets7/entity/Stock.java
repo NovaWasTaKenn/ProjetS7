8859c0bf468dbad6f112985522a811ec272491e7
Quentin LE NESTOUR <quentin.le_nestour@edu.devinci.fr>
2023-12-02 18:25:25 +0100

    progress on hibernate

---
+++ src/main/java/com/example/projets7/entity/Stock.java
@@ -0,0 +1,7 @@
+package com.example.projets7.entity;
+
+public class Stock {
+
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,12 @@ module com.example.projets7 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.hibernate.orm.core;
 
 
     opens com.example.projets7 to javafx.fxml;
     exports com.example.projets7.entity;
-    opens com.example.projets7.entity to javafx.fxml;
+    opens com.example.projets7.entity to org.hibernate.orm.core;
     exports com.example.projets7.util;
     opens com.example.projets7.util to javafx.fxml;
     exports com.example.projets7.controller;
